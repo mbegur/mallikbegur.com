d11ffe97a548b922da91acec8fc13b328276e584
Mallik Begur <mbegur@gmail.com>
2017-08-31 13:44:32 -0700

    fix jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,9 +27,10 @@
 			$body.addClass('is-loading');
 
 			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
+				$body.removeClass('is-loading');
+				// window.setTimeout(function() {
+				//
+				// }, 100);
 			});
 
 		// Mobile?
